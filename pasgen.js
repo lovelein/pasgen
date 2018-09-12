c265ef60546337c373a47c447856c4285cc4acd2
Vladislav <lovelein60@gmail.com>
2018-09-12 19:29:18 +0300

    version 0.1

---
+++ pasgen.js
@@ -0,0 +1,65 @@
+const chars= [
+    [ "a"],
+    [ "ae" ],
+    [ "ah" ],
+    [ "ai"],
+    [ "b" ],
+    [ "c" ],
+    [ "ch" ],
+    [ "d"],
+    [ "e" ],
+    [ "ee" ],
+    [ "ei" ],
+    [ "f" ],
+    [ "g" ],
+    [ "gh" ],
+    [ "h" ],
+    [ "i" ],
+    [ "ie" ],
+    [ "j" ],
+    [ "k" ],
+    [ "l" ],
+    [ "m" ],
+    [ "n" ],
+    [ "ng" ],
+    [ "o" ],
+    [ "oh" ],
+    [ "oo"],
+    [ "p" ],
+    [ "ph" ],
+    [ "qu" ],
+    [ "r" ],
+    [ "s"],
+    [ "sh" ],
+    [ "t" ],
+    [ "th"],
+    [ "u"],
+    [ "v"],
+    [ "w" ],
+    [ "x" ],
+    [ "y" ],
+    [ "z" ]
+];
+const num = '0123456789';
+const sym = '!@#$%^&*=-_';
+
+const charNum = document.getElementById("charNum");
+const numBox = document.getElementById("num");
+const symBox = document.getElementById("sym");
+const submit = document.getElementById("submit");
+const yourPw = document.getElementById("yourPw");
+
+submit.addEventListener("click",function(e){
+    let characters = chars;
+    (numBox.checked) ? characters += num : '';
+    (symBox.checked) ? characters += sym : '';
+    yourPw.value = pas(charNum.value, characters);
+});
+
+let pas = (length,characters)=>  {
+    let pas = "";
+    for(let i = 0; i<length; i++){
+        pas += characters[(Math.floor(Math.random() * characters.length))];
+    }
+    return pas;
+};
