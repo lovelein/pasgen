6c8a5b1dca7931d11d3567d967be208f4a92292e
Vladislav <lovelein60@gmail.com>
2018-09-16 22:08:21 +0300

    final commit

--- pasgen.js
+++ pasgen.js
@@ -1,60 +1,57 @@
 const chars= [
-    [ "a"],
-    [ "ae" ],
-    [ "ah" ],
-    [ "ai"],
-    [ "b" ],
-    [ "c" ],
-    [ "ch" ],
-    [ "d"],
-    [ "e" ],
-    [ "ee" ],
-    [ "ei" ],
-    [ "f" ],
-    [ "g" ],
-    [ "gh" ],
-    [ "h" ],
-    [ "i" ],
-    [ "ie" ],
-    [ "j" ],
-    [ "k" ],
-    [ "l" ],
-    [ "m" ],
-    [ "n" ],
-    [ "ng" ],
-    [ "o" ],
-    [ "oh" ],
-    [ "oo"],
-    [ "p" ],
-    [ "ph" ],
-    [ "qu" ],
-    [ "r" ],
-    [ "s"],
-    [ "sh" ],
-    [ "t" ],
-    [ "th"],
-    [ "u"],
-    [ "v"],
-    [ "w" ],
-    [ "x" ],
-    [ "y" ],
-    [ "z" ]
+    [ "thumb"],
+    [ "cart" ],
+    [ "minute" ],
+    [ "board"],
+    [ "flood" ],
+    [ "unequaled" ],
+    [ "stroke" ],
+    [ "note"],
+    [ "measure" ],
+    [ "crooked" ],
+    [ "acrid" ],
+    [ "kick" ],
+    [ "cemetery" ],
+    [ "reward" ],
+    [ "hang" ],
+    [ "rail" ],
+    [ "rainstorm" ],
+    [ "request" ],
+    [ "force" ],
+    [ "train" ],
+    [ "watch" ],
+    [ "tangy" ],
+    [ "precede" ],
+    [ "wren" ],
+    [ "toy" ],
+    [ "dirt"],
+    [ "wonderful" ],
+    [ "ossified" ],
+    [ "sudden" ],
+    [ "brass" ],
+    [ "part"],
+    [ "quack" ],
+    [ "chop" ],
+    [ "short"],
+    [ "liquid"],
+    [ "solid"],
+    [ "breezy" ],
+    [ "milk" ],
+    [ "bread" ],
+    [ "serious" ]
 ];
-const num = '0123456789';
 
 const charNum = document.getElementById("charNum");
-const numBox = document.getElementById("num");
 const submit = document.getElementById("submit");
 const yourPw = document.getElementById("yourPw");
 
 submit.addEventListener("click",function(e){
-    let characters = chars;
-    (numBox.checked) ? characters += num : '';
-    yourPw.value = pas(charNum.value, characters);
+    yourPw.value = pas(charNum.value, chars);
 });
 
 let pas = (length,characters)=>  {
     let pas = "";
+
     for(let i = 0; i<length; i++){
         pas += characters[(Math.floor(Math.random() * characters.length))];
     }
